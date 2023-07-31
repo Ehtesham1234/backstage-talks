e9021b4a5d9e21e5919e0e1acbc0855b8db1976b
Ehtesham1234 <ehteshamusman12344321@gmail.com>
2023-08-01 00:06:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 // App.js
-import React, { useState, useEffect, useRef } from 'react';
-import './App.css';
-import Header from './Header';
-import Body from './Body';
-import Footer from './Footer';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
 
 function App() {
-  const [bgColor, setBgColor] = useState('white');
-  const colors = ['#ff608c', '#ffffff', '#00c1b5', '#ff6519', '#ffbe00', '#e30512', '#1d3fbb'];
+  const [bgColor, setBgColor] = useState("white");
+  const colors = [
+    "#ff608c",
+    "#ffffff",
+    "#00c1b5",
+    "#ff6519",
+    "#ffbe00",
+    "#e30512",
+    "#1d3fbb",
+  ];
 
   const handleScroll = useRef((event) => {
-    const cards = document.getElementsByClassName('body__card');
+    const cards = document.getElementsByClassName("body__card");
     const scrollPosition = window.scrollY + window.innerHeight / 2;
 
     for (let i = 0; i < cards.length; i++) {
       const card = cards[i];
       const cardTop = card.getBoundingClientRect().top + window.scrollY;
 
-      if (scrollPosition >= cardTop && scrollPosition < cardTop + card.offsetHeight) {
+      if (
+        scrollPosition >= cardTop &&
+        scrollPosition < cardTop + card.offsetHeight
+      ) {
         setBgColor(colors[i % colors.length]);
         break;
       }
@@ -25,9 +36,9 @@ function App() {
   }).current;
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [handleScroll]);
 
